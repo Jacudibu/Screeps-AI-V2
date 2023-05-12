45edf0c17123db315a48dc74171f67a9543d858a
jacudibu <jacudibu@gmail.com>
2023-05-12 21:28:13 +0200

    Trying to get some stuff moving

--- src/main.js
+++ src/main.js
@@ -1,3 +1,23 @@
+require('tools.traveler')
+
+if (!Memory.creepsBuilt) {
+    Memory.creepsBuilt = 0;
+}
+
 module.exports.loop = function () {
-    
+    Game.spawns['Spawn1'].spawnCreep( [WORK, CARRY, MOVE], 'Harvester1' );
+
+    var creep = Game.creeps['Harvester'];
+
+    if(creep.store.getFreeCapacity() > 0) {
+        var sources = creep.room.find(FIND_SOURCES);
+        if(creep.harvest(sources[0]) === ERR_NOT_IN_RANGE) {
+            creep.moveTo(sources[0]);
+        }
+    }
+    else {
+        if( creep.transfer(Game.spawns['Spawn1'], RESOURCE_ENERGY) === ERR_NOT_IN_RANGE ) {
+            creep.moveTo(Game.spawns['Spawn1']);
+        }
+    }
 };
