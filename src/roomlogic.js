5fe2112c600f8d098a81ff7be99c2b7d331ed865
jacudibu <jacudibu@gmail.com>
2023-05-13 19:36:21 +0200

    Spawncode is only triggered when spawns are idle

--- src/roomlogic.js
+++ src/roomlogic.js
@@ -10,28 +10,41 @@ const roomLogic = {
 
     updateOwnedRoom(room) {
         room.checkForRCLUpdate();
+        this.processSpawnQueue(room);
+    },
 
-        const spawns = room.find(FIND_MY_SPAWNS);
-        _.forEach(spawns, spawn => {
-            const body = [WORK, CARRY, MOVE];
-            const name = Memory.creepsBuilt.toString();
-            const memory = {role: ROLE.RCL1_CREEP};
-
-            const result = spawn.spawnCreep(body, name, {memory: memory});
-            switch (result) {
-                case OK:
-                    Memory.creepsBuilt = Memory.creepsBuilt + 1;
-                    break;
-                case ERR_NOT_ENOUGH_ENERGY:
-                    break;
-                default:
-                    log.warning(room + " unexpected error when spawning creep: " + result
-                        + "\nBody: " + body.length + " -> " + body + "\nname:" + name + "\nmemory:" + memory);
-                    break;
+    processSpawnQueue(room) {
+        const idleSpawns = room.find(FIND_MY_SPAWNS, {
+            filter: spawn => {
+                return spawn.isActive() && spawn.spawning === null
             }
         });
-    }
 
+        if (idleSpawns.length === 0) {
+            return;
+        }
+
+        this.spawnSomething(room, idleSpawns[0]);
+    },
+
+    spawnSomething(room, spawn) {
+        const body = [WORK, CARRY, MOVE];
+        const name = Memory.creepsBuilt.toString();
+        const memory = {role: ROLE.RCL1_CREEP};
+
+        const result = spawn.spawnCreep(body, name, {memory: memory});
+        switch (result) {
+            case OK:
+                Memory.creepsBuilt = Memory.creepsBuilt + 1;
+                break;
+            case ERR_NOT_ENOUGH_ENERGY:
+                break;
+            default:
+                log.warning(room + " unexpected error when spawning creep: " + result
+                    + "\nBody: " + body.length + " -> " + body + "\nname:" + name + "\nmemory:" + memory);
+                break;
+        }
+    }
 }
 
 profiler.registerObject(roomLogic, "roomLogic");
