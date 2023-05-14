85eb11f72d73a8f46210bda01999800940660429
jacudibu <jacudibu@gmail.com>
2023-05-14 09:47:33 +0200

    refactoring

--- src/roomai.js
+++ src/roomai.js
@@ -5,12 +5,24 @@ const ownedRoom = {
         for (let roomName in Game.rooms) {
             let room = Game.rooms[roomName];
             if (room.controller && room.controller.my) {
-                this.updateOwnedRoom(room);
+                this._tryRunRoomLogic(room);
             }
         }
     },
 
-    updateOwnedRoom(room) {
+    _tryRunRoomLogic(room) {
+        try {
+            this._runRoomLogic(room);
+        } catch (e) {
+            let message = room + " -> caught error: " + e;
+            if (e.stack) {
+                message += "\nTrace:\n" + e.stack;
+            }
+            log.error(message);
+        }
+    },
+
+    _runRoomLogic: function(room) {
         room.checkForRCLUpdate();
         spawnLogic.run(room);
     },
