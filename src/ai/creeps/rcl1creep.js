a120b9495627e78d714b9f0b89bd2deff44a21ce
jacudibu <jacudibu@gmail.com>
2023-05-14 15:41:09 +0200

    rcl1 creeps more aggressively search for new paths when unable to find a path to their source

--- src/ai/creeps/rcl1creep.js
+++ src/ai/creeps/rcl1creep.js
@@ -22,7 +22,7 @@ const rcl1creep = {
         }
 
         if(creep.harvest(source) === ERR_NOT_IN_RANGE) {
-            creep.travelTo(source);
+            creep.travelTo(source, {stuckValue: 1});
         }
     },
 
