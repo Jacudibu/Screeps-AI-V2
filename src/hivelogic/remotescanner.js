754d3cfcd0a0f1f0692abb3dc4b99535906dee16
jacudibu <jacudibu@gmail.com>
2023-05-19 07:34:16 +0200

    highways are now considered for remote pathing

--- src/hivelogic/remotescanner.js
+++ src/hivelogic/remotescanner.js
@@ -6,9 +6,6 @@ class RemoteScanner {
         // This currently hardcodes remote distance to 2, but that shouldn't be a problem...
         for (const dir in mainRoomNeighbors) {
             const roomName = mainRoomNeighbors[dir];
-            if (utils.isRoomHighway(roomName)) {
-                continue;
-            }
 
             neighborRooms.push({name: roomName, distance: 1});
             const neighborNeighbors = Game.map.describeExits(roomName);
@@ -22,16 +19,16 @@ class RemoteScanner {
                     continue;
                 }
 
-                if (utils.isRoomHighway(neighborName)) {
-                    continue;
-                }
-
                 neighborRooms.push({name: neighborName, distance: 2});
             }
         }
 
         hive.remotes = {};
         for (const r of neighborRooms) {
+            if (utils.isRoomHighway(r.name)) {
+                continue;
+            }
+
             hive.remotes[r.name] = {
                 distance: r.distance,
             };
@@ -45,6 +42,36 @@ class RemoteScanner {
                 s => PathFinder.search(s.pos, hiveRoomStoragePosition, {maxRooms: remoteData.distance + 1}).path.length); // TODO: This ignores potential tunnels
 
         remoteData.max_early_workers = _.sum(remoteData.sourceDistance, dist => 1 + Math.trunc(dist / 50));
+    }
+
+    static orderRemotesByValue(hive) {
+        const oldData = hive.remotes;
+        const values = [];
+
+        for (const name in oldData) {
+            values.push({name: name, data: oldData[name]})
+        }
+
+        _.sortBy(values, v => _.min(v.data.sources));
+
+        const newData = {};
+        for (const value of values) {
+            newData[value.name] = value.data;
+        }
+
+        return newData;
+    }
+}
+
+global.resetHiveRemotes = function() {
+    for (const hiveName in Hives) {
+        RemoteScanner.setupRemoteData(Hives[hiveName]);
+    }
+}
+
+global.debugOrdering = function() {
+    for (const hiveName in Hives) {
+        log.info(JSON.stringify(RemoteScanner.orderRemotesByValue(Hives[hiveName]), null,  2));
     }
 }
 
