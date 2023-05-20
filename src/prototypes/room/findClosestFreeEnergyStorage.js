f878d8220f33475091fff52208b30765da3ea020
jacudibu <jacudibu@gmail.com>
2023-05-20 14:33:14 +0200

    haulers now pick up dropped energy and haul them somewhere

--- src/prototypes/room/findClosestFreeEnergyStorage.js
+++ src/prototypes/room/findClosestFreeEnergyStorage.js
@@ -1,31 +1,35 @@
 Room.prototype.findClosestFreeEnergyStorageTo = function(pos, amount) {
-    const towersBelowThreshold = this.myTowers.filter(tower => tower.energy < tower.energyCapacity * 0.25);
+    const towersBelowThreshold = this.myTowers.filter(x => x.energy < x.energyCapacity * 0.25);
     if (towersBelowThreshold.length > 0) {
         return Utils.getClosestRoomObjectToPosition(pos, towersBelowThreshold);
     }
 
-    let spawns = this.mySpawns.filter(spawn => spawn.energy < spawn.energyCapacity);
+    let spawns = this.mySpawns.filter(x => x.energy < x.energyCapacity);
     if (spawns.length > 0) {
         return Utils.getClosestRoomObjectToPosition(pos, spawns);
     }
 
-    let extension = this.getClosestEmptyExtensionToPosition(this, amount);
-    if (extension !== ERR_NOT_FOUND) {
-        return extension;
+    const extensions = this.myExtensions.filter(x => x.energy < x.energyCapacity);
+    if (extensions.length > 0) {
+        return Utils.getClosestRoomObjectToPosition(pos, extensions);
     }
+
+    return null;
 };
 
 Room.prototype.getClosestEmptyExtensionToPosition = function(pos, energy = 0) {
-    if (this.freeExtensions.length === 0) {
-        return ERR_NOT_FOUND;
-    }
+    const extensions = this.myExtensions.filter(x => x.energy < x.energyCapacity);
 
-    let closestExtension = Utils.getClosestRoomObjectToPosition(pos, this.freeExtensions);
-
-    if (energy > (closestExtension.energyCapacity - closestExtension.energy)) {
-        _.remove(this._freeExtensions, extension => extension.id === closestExtension.id);
-        _.remove(freeRoomExtensions[this.name], extensionsId => extensionsId === closestExtension.id);
-    }
-
-    return closestExtension;
+    // if (this.freeExtensions.length === 0) {
+    //     return ERR_NOT_FOUND;
+    // }
+    //
+    // let closestExtension = Utils.getClosestRoomObjectToPosition(pos, this.freeExtensions);
+    //
+    // if (energy > (closestExtension.energyCapacity - closestExtension.energy)) {
+    //     _.remove(this._freeExtensions, extension => extension.id === closestExtension.id);
+    //     _.remove(freeRoomExtensions[this.name], extensionsId => extensionsId === closestExtension.id);
+    // }
+    //
+    // return closestExtension;
 };
